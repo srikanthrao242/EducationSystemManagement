1b5b8b74298da332f0587101738b096b042bd190
srikanthrao242 <srikanthraomca@yahoo.com>
2019-12-26 07:47:13 +0530

    added recover for exception in user adding

--- src/main/scala/org/ems/services/RegisterService.scala
+++ src/main/scala/org/ems/services/RegisterService.scala
@@ -6,16 +6,26 @@ import org.ems.entities.RegisterUser
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class RegisterService(userService: UserService, companyService : CompanyService)(implicit ec: ExecutionContext) extends SLF4JLogging{
+class RegisterService(userService: UserService, companyService: CompanyService)(
+  implicit ec: ExecutionContext
+) extends SLF4JLogging {
 
-  def registerUser(registerUser: RegisterUser): Future[(Int,Int)] ={
-    for{
+  def registerUser(registerUser: RegisterUser): Future[(Int, Int)] =
+    for {
       company <- companyService.addCompany(registerUser.company)
-      user <- userService.addUser(registerUser.user.copy(`companyid` = Some(company)))
-    }yield {
+      user <- userService
+        .addUser(registerUser.user.copy(`companyid` = Some(company)))
+        .recoverWith {
+          case ex: Exception =>
+            log.error(s"Got error while adding user so deleting company with id $company")
+            for {
+              v <- companyService.deleteCompany(company)
+            } yield v
+            throw ex
+        }
+    } yield {
       log.debug(s"added company and user with $company, $user")
       (company, user)
     }
-  }
 
 }
