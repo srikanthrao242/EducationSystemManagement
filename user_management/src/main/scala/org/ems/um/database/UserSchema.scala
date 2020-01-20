2a36662a81fde2de4239bb1dcf95f85101854e84
srikanthrao242 <srikanthraomca@yahoo.com>
2020-01-21 00:00:54 +0530

    new schemas added to create Admissions

--- user_management/src/main/scala/org/ems/um/database/UserSchema.scala
+++ user_management/src/main/scala/org/ems/um/database/UserSchema.scala
@@ -24,6 +24,14 @@ trait UserSchema extends SLF4JLogging {
       _ <- createCreditDetails(schema).unsafeToFuture()
       _ <- createTimeSheet(schema).unsafeToFuture()
       _ <- createHolidays(schema).unsafeToFuture()
+      _ <- createAcademic(schema).unsafeToFuture()
+      _ <- createStudentDetails(schema).unsafeToFuture()
+      _ <- createParentDetails(schema).unsafeToFuture()
+      _ <- createEdQualifications(schema).unsafeToFuture()
+      _ <- createSections(schema).unsafeToFuture()
+      _ <- createClasses(schema).unsafeToFuture()
+      _ <- createAdmissionDetails(schema).unsafeToFuture()
+      _ <- createAdmissionFeeDetails(schema).unsafeToFuture()
     } yield {
       log.debug(s"successfully created employee table")
       eid
@@ -221,6 +229,215 @@ trait UserSchema extends SLF4JLogging {
          |    REFERENCES `educationmanagementsystem`.`user` (`id`)
          |    ON DELETE NO ACTION
          |    ON UPDATE NO ACTION);
+         |""".stripMargin
+    log.debug(query.toString)
+    DbModule.transactor.use { xa =>
+      Update(query).run().transact(xa)
+    }
+  }
+
+  def createStudentDetails(db: String): IO[Int] = {
+    val query =
+      s"""
+         |CREATE TABLE `$db`.`student_details` (
+         |  `StudentID` INT NOT NULL AUTO_INCREMENT ,
+         |  `FirstName` VARCHAR(100) NULL,
+         |  `LastName` VARCHAR(100) NULL,
+         |  `DOB` DATETIME NULL,
+         |  `BloodGroup` VARCHAR(45) NULL,
+         |  `Gender` ENUM('Male', 'Female') NULL,
+         |  `Religion` VARCHAR(45) NULL,
+         |  `Address` VARCHAR(150) NULL,
+         |  `City` VARCHAR(45) NULL,
+         |  `State` VARCHAR(45) NULL,
+         |  `PinCode` VARCHAR(45) NULL,
+         |  `Mobile` VARCHAR(45) NULL,
+         |  `Country` VARCHAR(45) NULL,
+         |  `Email` VARCHAR(45) NULL,
+         |  `IsActive` TINYINT NULL,
+         |  `ProfileImage` VARCHAR(200) NULL,
+         |  PRIMARY KEY (`StudentID`),
+         |  UNIQUE INDEX `StudentID_UNIQUE` (`StudentID` ASC) VISIBLE);
+         |""".stripMargin
+    log.debug(query.toString)
+    DbModule.transactor.use { xa =>
+      Update(query).run().transact(xa)
+    }
+  }
+
+  def createParentDetails(db: String): IO[Int] = {
+    val query =
+      s"""
+         |CREATE TABLE `$db`.`parent_details` (
+         |  `ID` INT NOT NULL AUTO_INCREMENT ,
+         |  `FirstName` VARCHAR(45) NULL,
+         |  `LastName` VARCHAR(45) NULL,
+         |  `Occupation` VARCHAR(45) NULL,
+         |  `Qualification` VARCHAR(45) NULL,
+         |  `StudentID` INT NULL,
+         |  `Religion` VARCHAR(45) NULL,
+         |  `Address` VARCHAR(100) NULL,
+         |  `City` VARCHAR(50) NULL,
+         |  `State` VARCHAR(45) NULL,
+         |  `PinCode` VARCHAR(45) NULL,
+         |  `Country` VARCHAR(45) NULL,
+         |  `Mobile` VARCHAR(45) NULL,
+         |  `Email` VARCHAR(45) NULL,
+         |  PRIMARY KEY (`ID`),
+         |  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+         |  INDEX `parent_student_idx` (`StudentID` ASC) VISIBLE,
+         |  CONSTRAINT `parent_student`
+         |    FOREIGN KEY (`StudentID`)
+         |    REFERENCES `$db`.`student_details` (`StudentID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION);
+         |
+         |""".stripMargin
+    log.debug(query.toString)
+    DbModule.transactor.use { xa =>
+      Update(query).run().transact(xa)
+    }
+  }
+
+  def createEdQualifications(db: String): IO[Int] = {
+    val query =
+      s"""
+         |CREATE TABLE `$db`.`education_qualification` (
+         |  `EdID` INT NOT NULL AUTO_INCREMENT,
+         |  `StudentID` INT NULL,
+         |  `InstitutionName` VARCHAR(100) NULL,
+         |  `Course` VARCHAR(45) NULL,
+         |  `Year` YEAR(4) NULL,
+         |  `TotalMarksObtained` DOUBLE NULL,
+         |  PRIMARY KEY (`EdID`),
+         |  INDEX `ed_student_idx` (`StudentID` ASC) VISIBLE,
+         |  CONSTRAINT `ed_student`
+         |    FOREIGN KEY (`StudentID`)
+         |    REFERENCES `$db`.`student_details` (`StudentID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION);
+         |""".stripMargin
+
+    log.debug(query.toString)
+    DbModule.transactor.use { xa =>
+      Update(query).run().transact(xa)
+    }
+  }
+
+  def createSections(db: String): IO[Int] = {
+    val query =
+      s"""
+         |CREATE TABLE `$db`.`class_sections` (
+         |  `SectionID` INT NOT NULL AUTO_INCREMENT,
+         |  `SectionName` VARCHAR(45) NULL,
+         |  `TakeCarer` INT NULL,
+         |  `RoomDetails` VARCHAR(45) NULL,
+         |  PRIMARY KEY (`SectionID`));
+         |""".stripMargin
+    log.debug(query.toString)
+    DbModule.transactor.use { xa =>
+      Update(query).run().transact(xa)
+    }
+  }
+
+  def createClasses(db: String): IO[Int] = {
+    val query =
+      s"""
+         |CREATE TABLE `$db`.`classes` (
+         |  `ClassID` INT NOT NULL AUTO_INCREMENT,
+         |  `ClassName` VARCHAR(45) NULL,
+         |  `AcademicID` INT NULL,
+         |  `SectionID` INT NULL,
+         |  `NumberOfSections` INT NULL,
+         |  PRIMARY KEY (`ClassID`),
+         |  INDEX `academic_class_idx` (`AcademicID` ASC) VISIBLE,
+         |  INDEX `section_class_idx` (`SectionID` ASC) VISIBLE,
+         |  CONSTRAINT `academic_class`
+         |    FOREIGN KEY (`AcademicID`)
+         |    REFERENCES `$db`.`academic_details` (`AcademicID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION,
+         |  CONSTRAINT `section_class`
+         |    FOREIGN KEY (`SectionID`)
+         |    REFERENCES `$db`.`class_sections` (`SectionID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION);
+         |
+         |""".stripMargin
+    log.debug(query.toString)
+    DbModule.transactor.use { xa =>
+      Update(query).run().transact(xa)
+    }
+  }
+
+  def createAdmissionDetails(db: String): IO[Int] = {
+    val query =
+      s"""
+         |CREATE TABLE `$db`.`admission` (
+         |  `AdmissionID` INT NOT NULL AUTO_INCREMENT,
+         |  `StudentID` INT NULL,
+         |  `ClassID` INT NULL,
+         |  `SectionID` INT NULL,
+         |  `AdmissionDate` TIMESTAMP NULL,
+         |  `AcademicID` INT NULL,
+         |  PRIMARY KEY (`AdmissionID`),
+         |  INDEX `admision_student_idx` (`StudentID` ASC) VISIBLE,
+         |  INDEX `admision_class_idx` (`ClassID` ASC) VISIBLE,
+         |  INDEX `admision_section_idx` (`SectionID` ASC) VISIBLE,
+         |  INDEX `admision_academic_idx` (`AcademicID` ASC) VISIBLE,
+         |  CONSTRAINT `admision_student`
+         |    FOREIGN KEY (`StudentID`)
+         |    REFERENCES `$db`.`student_details` (`StudentID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION,
+         |  CONSTRAINT `admision_class`
+         |    FOREIGN KEY (`ClassID`)
+         |    REFERENCES `$db`.`classes` (`ClassID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION,
+         |  CONSTRAINT `admision_section`
+         |    FOREIGN KEY (`SectionID`)
+         |    REFERENCES `$db`.`class_sections` (`SectionID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION,
+         |  CONSTRAINT `admision_academic`
+         |    FOREIGN KEY (`AcademicID`)
+         |    REFERENCES `$db`.`academic_details` (`AcademicID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION);
+         |
+         |""".stripMargin
+
+    log.debug(query.toString)
+    DbModule.transactor.use { xa =>
+      Update(query).run().transact(xa)
+    }
+  }
+
+  def createAdmissionFeeDetails(db: String): IO[Int] = {
+    val query =
+      s"""
+         |CREATE TABLE `$db`.`admission_fee_details` (
+         |  `ID` INT NOT NULL AUTO_INCREMENT,
+         |  `AdmissionID` INT NULL,
+         |  `StudentID` INT NULL,
+         |  `AdmisionFee` DOUBLE NULL,
+         |  `PaidFee` DOUBLE NULL,
+         |  `PaidDate` TIMESTAMP NULL,
+         |  PRIMARY KEY (`ID`),
+         |  INDEX `admissionfee_admision_idx` (`AdmissionID` ASC) VISIBLE,
+         |  INDEX `admisionfee_studen_idx` (`StudentID` ASC) VISIBLE,
+         |  CONSTRAINT `admissionfee_admision`
+         |    FOREIGN KEY (`AdmissionID`)
+         |    REFERENCES `$db`.`admission` (`AdmissionID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION,
+         |  CONSTRAINT `admisionfee_studen`
+         |    FOREIGN KEY (`StudentID`)
+         |    REFERENCES `$db`.`student_details` (`StudentID`)
+         |    ON DELETE NO ACTION
+         |    ON UPDATE NO ACTION);
+         |
          |""".stripMargin
     log.debug(query.toString)
     DbModule.transactor.use { xa =>
